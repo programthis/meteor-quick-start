81d1f9f41e916128e94edf858bff9e2e626b97bf
programthis <kylebachan@gmail.com>
2017-05-10 19:44:47 -0400

    Added sample code to app

--- imports/startup/client/router.js
+++ imports/startup/client/router.js
@@ -4,9 +4,27 @@ import { BlazeLayout } from 'meteor/kadira:blaze-layout';
 import '../../ui/layout/layout.js';
 import '../../ui/home/home.js';
 
+// document.title = "Title of Website";
+
+// Sample setup for private routes
+// let privateRoutes = FlowRouter.group({
+// 	name: "private",
+// 	triggersEnter: [
+// 		isUser
+// 	]
+// });
+
+
 FlowRouter.route('/', {
 	name: 'home',
 	action() {
 		BlazeLayout.render('layout', { main: 'home' });
 	}
 });
+
+// Sample setup to determine if user is logged in
+// function isUser() {
+// 	if (!Meteor.userId()) {
+// 		FlowRouter.go("home");
+// 	}
+// }
--- imports/ui/home/home.js
+++ imports/ui/home/home.js
@@ -1,1 +1,15 @@
+import { Template } from 'meteor/templating';
+
 import "./home.html";
+
+// Sample setup for the home templates
+// Template.home.onCreated({
+// 	let self = this;
+// 	self.autorun(function() {
+// 		self.subscribe("users");
+// 	});
+// });
+
+// Template.home.events({
+
+// })
