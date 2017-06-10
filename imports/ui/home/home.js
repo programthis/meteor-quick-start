e9f61a57189dbfb0112a0568e183e4b8714cbe4e
programthis <kylebachan@gmail.com>
2017-06-10 12:47:40 -0400

    Commented out sample code

--- imports/ui/home/home.js
+++ imports/ui/home/home.js
@@ -3,13 +3,13 @@ import { Template } from 'meteor/templating';
 import "./home.html";
 
 // Sample setup for the home templates
-Template.home.onCreated(function() {
-	let self = this;
-	self.autorun(function() {
-		self.subscribe("users");
-	});
-});
+// Template.home.onCreated(function() {
+// 	let self = this;
+// 	self.autorun(function() {
+// 		self.subscribe("users");
+// 	});
+// });
 
-Template.home.events({
+// Template.home.events({
 
-})
+// })
