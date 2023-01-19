6299ba76eb5d6953a4645aa6b946a37e94a122ba
programthis <kylebachan@gmail.com>
2023-01-19 15:42:54 -0500

    Added scripts to make starting server and update quicker

---
+++ start.sh
@@ -0,0 +1,4 @@
+# run chmod 777 for this script to work
+
+echo "Starting Meteor development server"
+meteor --settings settings-development.json
---
+++ update.sh
@@ -0,0 +1,7 @@
+# run chmod 777 for this script to work
+
+echo "Running Meteor update"
+meteor update
+
+echo "Running Meteor npm install"
+meteor npm install
